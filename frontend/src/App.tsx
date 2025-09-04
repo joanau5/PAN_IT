a046d73906fdc7a251fee6fd3007bcc9622444e5
joanau5 <joanau@uci.edu>
2025-09-04 19:42:51 +0700

    Set up React, TS, Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind is working!
+      </h1>
+      <p className="text-gray-700 mt-2">
+      </p>
+    </div>
+  );
+}
+
+export default App;
